17f90b1fb16353e775cbd78d92b5f1f7bb146b1f
Anand Verma <anandverma458@gmail.com>
2024-01-12 20:18:29 +0530

    Update ncs.glsl

--- ncs.glsl
+++ ncs.glsl
@@ -1,6 +1,6 @@
 //1. Particle size and number
 #define numParticles ivec2(screen.xy)//Higher is slower. Leave it as is to render maximum number of particles (screen.x*screen.y)
-#define particleSize 4 //Higher is slower. Radius of the individual particles. Minimum 1
+#define particleSize 3 //Higher is slower. Radius of the individual particles. Minimum 1
 
 //2. Particle Color, Opacity and Glow
 #define color vec3(0.0118, 0.1412, 0.3412)//Color of the particles  //vec3(.0431,.1882,.8314); vec3(.2039,.051,.4941) vec3(.349,0.,1.) vec3(0.0, 0.3176, 1.0)(opacity=.03, cAS=0,op=.05,cAS=.04) vec3(1.0, 0.0, 0.298) (opacity=.2) vec3(0.1961, 0.0, 0.4549)(opacity=.05,cAS=.1) vec3(0.4196, 0.0314, 0.149)(op=.53,cAS=.13) vec3(0.0078, 0.0235, 0.0549),(op=.02,cAS=.6) vec3(0.6667, 0.0784, 0.0784) (op=0.03,cAS=.47), vec3(0.0118, 0.0196, 0.0157)(op=0.18,cAS=.6) vec3(0.0118, 0.1412, 0.3412)(op=.05,cAS=.2)
